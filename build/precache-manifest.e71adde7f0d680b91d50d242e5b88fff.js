f43afda721c6a237808d32243e5a1d54779aa74e
chrissyalbert <chrissy_albert@yahoo.com>
2020-05-02 16:33:32 -0700

    update .env

---
+++ build/precache-manifest.e71adde7f0d680b91d50d242e5b88fff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1188908228a59402c32851dd0daaa493",
+    "url": "/Covid-19-app/index.html"
+  },
+  {
+    "revision": "119ce8268e121a50e1e8",
+    "url": "/Covid-19-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b6c8f11a9d7a46f7d3fa",
+    "url": "/Covid-19-app/static/css/main.d5d0085a.chunk.css"
+  },
+  {
+    "revision": "119ce8268e121a50e1e8",
+    "url": "/Covid-19-app/static/js/2.ef4dede5.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/Covid-19-app/static/js/2.ef4dede5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c8f11a9d7a46f7d3fa",
+    "url": "/Covid-19-app/static/js/main.460244aa.chunk.js"
+  },
+  {
+    "revision": "749c8f386d5362e5ad54",
+    "url": "/Covid-19-app/static/js/runtime-main.14bc71a7.js"
+  }
+]);
