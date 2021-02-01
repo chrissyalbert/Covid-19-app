6c683b2863d718a7d592521984d762e6c20b5963
chrissyalbert <chrissy_albert@yahoo.com>
2021-01-31 19:17:09 -0700

    update node modules

---
+++ build/precache-manifest.89e6429e8ba1e7a445a4ac7c64288a3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15c9ad08baeea86b9a69ac5c954c80d",
+    "url": "/Covid-19-app/index.html"
+  },
+  {
+    "revision": "73777866c3390969b845",
+    "url": "/Covid-19-app/static/css/2.fe51dd2d.chunk.css"
+  },
+  {
+    "revision": "76a7cf9a65356ec7f896",
+    "url": "/Covid-19-app/static/css/main.d5d0085a.chunk.css"
+  },
+  {
+    "revision": "73777866c3390969b845",
+    "url": "/Covid-19-app/static/js/2.dfe2b6fb.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/Covid-19-app/static/js/2.dfe2b6fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a7cf9a65356ec7f896",
+    "url": "/Covid-19-app/static/js/main.d1ea5204.chunk.js"
+  },
+  {
+    "revision": "749c8f386d5362e5ad54",
+    "url": "/Covid-19-app/static/js/runtime-main.14bc71a7.js"
+  }
+]);
